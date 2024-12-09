7070b58f632d217062d45b0a0c98372d9ebe0a6a
boranblt <mehmetdotboran@gmail.com>
2024-12-10 00:13:16 +0300

    son1

--- Assets/scripts/Door.cs
+++ Assets/scripts/Door.cs
@@ -1,12 +1,25 @@
 using UnityEngine;
 
-public class Door : MonoBehaviour
+public class KeyAndDoor : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    public GameObject kapý1; // Kapýyý temsil eden GameObject
+    private bool hasKey = false; // Anahtarýn alýnýp alýnmadýðýný kontrol etmek için
+
+    void OnTriggerEnter(Collider other)
     {
-        if (collision.CompareTag("Player"))
+        // Anahtar alýndýðýnda
+        if (other.CompareTag("key1"))
         {
-            Debug.Log("Door Opened!");
+            hasKey = true;
+            Destroy(other.gameObject); // Anahtarý yok et
+            Debug.Log("Anahtar alýndý!");
+        }
+
+        // Kapýya ulaþýrsa ve anahtar varsa
+        if (other.CompareTag("kapý1") && hasKey)
+        {
+            Destroy(kapý1); // Kapýyý yok et
+            Debug.Log("Kapý açýldý!");
         }
     }
 }
